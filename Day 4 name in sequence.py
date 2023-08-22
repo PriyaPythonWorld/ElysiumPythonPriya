921a1a72b0176d7cbb96f30d45e60c6905e2c2b0
PriyaPythonWorld <142863922+PriyaPythonWorld@users.noreply.github.com>
2023-08-22 09:30:26 +0530

    Day 4 name in sequence

---
+++ Day 4 name in sequence.py
@@ -0,0 +1,10 @@
+word = "MohammedNisar"
+length = len(word) + 1
+print(word)
+values = range(length)
+print(values)
+for i in values:
+    length = i
+    str = word [:length]
+    print(str)
+    #print(i)
